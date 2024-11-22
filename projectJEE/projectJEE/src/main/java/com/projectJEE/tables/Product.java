5db9c15aac5ead4618cd78183ad45006debc0c13
Rubenso <Rubens.tueux@cy-tech.fr>
2024-11-22 10:12:39 +0100

    bonne version

---
+++ projectJEE/projectJEE/src/main/java/com/projectJEE/tables/Product.java
@@ -0,0 +1,103 @@
+package com.projectJEE.tables;
+import java.util.List;
+import java.util.ArrayList;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Product {
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private Long id;
+	
+	private String name = "";
+	
+	private List<String> otherNames = new ArrayList<>();
+	
+	private ProductType type;
+	
+	private String description;	
+	
+	private String picLink;
+	
+	@OneToOne(mappedBy = "stockedProduct")
+	private Stock stock;	
+	
+	@OneToMany(mappedBy = "usedProduct",fetch = FetchType.EAGER)
+	private List<EffectAndPreparation> possibleEffectsAnfPreparations = new ArrayList<>();
+	
+
+	
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<String> getOtherNames() {
+		return otherNames;
+	}
+
+	public void setOtherNames(List<String> otherNames) {
+		this.otherNames = otherNames;
+	}
+
+	public ProductType getType() {
+		return type;
+	}
+
+	public void setType(ProductType type) {
+		this.type = type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPicLink() {
+		return picLink;
+	}
+
+	public void setPicLink(String picLink) {
+		this.picLink = picLink;
+	}
+	
+	public Stock getStock() {
+		return stock;
+	}
+
+	public void setStock(Stock stock) {
+		this.stock = stock;
+		if (stock != null && stock.getStockedProduct() != this) {
+	        stock.setStockedProduct(this);
+	    }
+	}
+	
+	public List<EffectAndPreparation> getPossibleEffectsAnfPreparations() {
+		return possibleEffectsAnfPreparations;
+	}
+
+	public void setPossibleEffectsAnfPreparations(List<EffectAndPreparation> possibleEffectsAnfPreparations) {
+		this.possibleEffectsAnfPreparations = possibleEffectsAnfPreparations;
+	}
+	
+}

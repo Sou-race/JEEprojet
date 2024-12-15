64270b83f9c2966edec609d4750100c66a08a879
Pierre-Antoine <103061131+Sou-race@users.noreply.github.com>
2024-12-15 22:48:37 +0100

    dark mode

--- projectJEE/src/main/java/com/projectJEE/controllers/ProductController.java
+++ projectJEE/src/main/java/com/projectJEE/controllers/ProductController.java
@@ -11,7 +11,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;

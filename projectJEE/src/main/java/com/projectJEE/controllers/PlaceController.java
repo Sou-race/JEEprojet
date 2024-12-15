385b260c2ec42a1a97821186ade06dff9f7fb2c3
Erra570 <148940999+Erra570@users.noreply.github.com>
2024-12-15 23:37:19 +0100

    tiens p-a (modifier supprimer ajouter localisation )

--- projectJEE/src/main/java/com/projectJEE/controllers/PlaceController.java
+++ projectJEE/src/main/java/com/projectJEE/controllers/PlaceController.java
@@ -18,43 +18,43 @@ import com.projectJEE.repositories.ProductRepository;
 import com.projectJEE.services.PlaceService;
 import com.projectJEE.tables.Place;
 import com.projectJEE.tables.Product;
-
 @Controller
 public class PlaceController {
 
-	@Autowired 
-	PlaceRepository placeRepository;
-	
-	@Autowired
-    PlaceService placeService;
-	
-	@Autowired
-	ProductRepository productRepository;
-	
-	@GetMapping("/places")
-	public String showPlacesById(Model model) {
-		List<Place> places = placeRepository.findAllByOrderByName();
-		model.addAttribute("places", places);
-		
-		List<Product> products = productRepository.findAll();
-        model.addAttribute("products", products);
-        
-		return "places";
-	}
-	
-	@PostMapping("/{placeId}/add-product")
-	@ResponseBody
-	public ResponseEntity<String> addProductToPlace(@PathVariable Long placeId, @RequestParam Long productId) {
-	    System.out.println("Adding product with ID: " + productId + " to place with ID: " + placeId);
-	    placeService.addProductToPlace(placeId, productId);
-	    return ResponseEntity.ok("Product added to place successfully");
-	}
-	
-	@DeleteMapping("/{placeId}/remove-product/{productId}")
-	@ResponseBody
-	public ResponseEntity<String> removeProductFromPlace(@PathVariable Long placeId, @PathVariable Long productId) {
-	    System.out.println("Removing product with ID: " + productId + " from place with ID: " + placeId);
-	    placeService.removeProductFromPlace(placeId, productId);
-	    return ResponseEntity.ok("Product removed from place successfully");
-	}
+    @Autowired
+    private PlaceRepository placeRepository;
+
+    @GetMapping("/places")
+    public String showPlaces(Model model) {
+        model.addAttribute("places", placeRepository.findAllByOrderByName());
+        return "places";
+    }
+
+    @GetMapping("/places/edit/{id}")
+    public String editPlace(@PathVariable Long id, Model model) {
+        Place place = placeRepository.findById(id).orElse(new Place());
+        model.addAttribute("placeToEdit", place);
+        model.addAttribute("places", placeRepository.findAllByOrderByName());
+        return "places";
+    }
+
+    @PostMapping("/places/save")
+    public String savePlace(@RequestParam(required = false) Long id,
+                            @RequestParam String name,
+                            @RequestParam int latitude,
+                            @RequestParam int longitude) {
+        Place place = (id != null) ? placeRepository.findById(id).orElse(new Place()) : new Place();
+        place.setName(name);
+        place.setLatitude(latitude);
+        place.setLongitude(longitude);
+        placeRepository.save(place);
+        return "redirect:/places";
+    }
+
+    @PostMapping("/places/delete")
+    public String deletePlace(@RequestParam Long id) {
+        placeRepository.deleteById(id);
+        return "redirect:/places";
+    }
 }
+

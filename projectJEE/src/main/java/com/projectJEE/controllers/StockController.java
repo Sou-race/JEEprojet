2bb55d7a8f2a6e8e60a5f1ce2e3f0c67134993b6
Erra570 <148940999+Erra570@users.noreply.github.com>
2024-12-15 22:28:08 +0100

    boutons add et remove stock
    non fonctionnels pour l'instant

--- projectJEE/src/main/java/com/projectJEE/controllers/StockController.java
+++ projectJEE/src/main/java/com/projectJEE/controllers/StockController.java
@@ -6,22 +6,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.projectJEE.repositories.StockRepository;
 import com.projectJEE.tables.Stock;
 
 @Controller
 public class StockController {
-	
-	@Autowired 
-	StockRepository stockRepository;
-	
-	@GetMapping("/stocks")
-	public String showStockByOrder(Model model) {
-		List<Stock> stocks = stockRepository.findAllByOrderByStockedProduct();		
-		model.addAttribute("stocks", stocks);
-		return "stocks";
-	}
-	
 
+    @Autowired
+    StockRepository stockRepository;
+
+    @GetMapping("/stocks")
+    public String showStockByOrder(Model model) {
+        List<Stock> stocks = stockRepository.findAllByOrderByStockedProduct();
+        model.addAttribute("stocks", stocks);
+        return "stocks";
+    }
+
+    @PostMapping("/stocks/add")
+    public String addStock(@RequestParam Long id) {
+        stockRepository.findById(id).ifPresent(stock -> {
+            if (stock.getDosage() != null) {
+                stock.getDosage().setQuantity(stock.getDosage().getQuantity() + 1);
+                stockRepository.save(stock);
+            }
+        });
+        return "redirect:/stocks";
+    }
+
+    @PostMapping("/stocks/remove")
+    public String removeStock(@RequestParam Long id) {
+        stockRepository.findById(id).ifPresent(stock -> {
+            if (stock.getDosage() != null && stock.getDosage().getQuantity() > 0) {
+                stock.getDosage().setQuantity(stock.getDosage().getQuantity() - 1);
+                stockRepository.save(stock);
+            }
+        });
+        return "redirect:/stocks";
+    }
 }
